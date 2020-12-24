fa20e69f19097cbca18a6dc6c0b7e249675e803c
kanabicks <3940348as@mail.ru>
2020-12-24 16:41:24 +0000

    init

---
+++ src/components/Container.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container"></div> 
+    <slot> </slot>  
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+.container {
+  width: 764px;
+  margin: auto;
+  box-shadow: 0 0 15px 0 #5f5f5f73;
+}
+
+</style>
