01e134d46865b1bb34e8cffe6a31c568cd70e66d
kanabicks <3943048as@mail.ru>
2021-01-14 18:56:03 +0000

    fix some bugs

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,62 @@
 <template>
   <div id="app">
     <ChatWindow>
-      <chat-message v-for="(message,i ) in messages" v-bind:key="i" username="Ivan" time="21.31.3434 21:21">
-    <Container><chat-message v-bind:username="kanabicks" datetime="11.111.2212">Hello!</chat-message></Container>
+      <chat-message
+        v-for="(message, i) in messages"
+        v-bind:key="i"
+        username="Ivan"
+        time="21.31.3434 21:21"
+      >
+        <Container
+          ><chat-message username="kanabicks" datetime="11.111.2212"
+            >Hello!</chat-message
+          ></Container
+        >
       </chat-message>
     </ChatWindow>
   </div>
 </template>
 
 <script>
-import ChatMessage from './components/ChatMessage.vue'
-import Container from './components/Container.vue'
-import ChatWindow from './components/ChatWindow'
+import ChatMessage from "./components/ChatMessage.vue";
+import Container from "./components/Container.vue";
+import ChatWindow from "./components/ChatWindow";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Container,
     ChatMessage,
-    ChatWindow
+    ChatWindow,
   },
-  data(){
-   // messages:[
-   //   {username: 'Ivan', datetime: '21.312.312' , text: 'dsdsdsd'},
-   //   {username: 'Ivan', datetime: '21.312.312' , text: 'dsdsdsd'}
-   // ]
-
+  data() {
+    return {
+      messages: [
+        { username: "Ivan", datetime: "21.312.312", text: "dsdsdsd" },
+        { username: "Ivan", datetime: "21.312.312", text: "dsdsdsd" },
+      ],
+    };
   },
-  methods:{
-    getMessages(){
-      this.axios.get('http://37.77.104.246/api/chat/getmessages.php')
-      .then((response)=>{
-        this.messages=response.data;
-      })
-    }
+  methods: {
+    getMessages() {
+      this.axios
+        .get("http://37.77.104.246/api/chat/getmessages.php")
+        .then((response) => {
+          this.messages = response.data;
+        });
+    },
   },
-  mounted(){
-     this.getMessages();
-  }
-}
+  mounted() {
+    this.getMessages();
+  },
+};
 </script>
 
 <style>
 #app {
-
-body {
-  margin: 0;
-  background-color: #f9f9fa;
-}
-
+  body {
+    margin: 0;
+    background-color: #f9f9fa;
+  }
 }
 </style>
