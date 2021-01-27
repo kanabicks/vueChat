5c474871a04bb9c6c36d5db73295c40f94c82653
kanabicks <47868136+kanabicks@users.noreply.github.com>
2021-01-27 18:37:21 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,63 @@
 <template>
   <div id="app">
-    <ChatWindow>
-      <chat-message v-for="(message,i ) in messages" v-bind:key="i" username="Ivan" time="21.31.3434 21:21">
-    <Container><chat-message v-bind:username="kanabicks" datetime="11.111.2212">Hello!</chat-message></Container>
-      </chat-message>
-    </ChatWindow>
+    <Container>
+      <ChatWindow>
+        <ChatMessage 
+          v-for="(message, i) in messages" 
+          v-bind:key="i"
+          
+          v-bind:username="message.author" 
+          v-bind:datetime="message.datetime" 
+        >
+          {{message.text}}
+        </ChatMessage>
+      </ChatWindow>
+    </Container>
   </div>
 </template>
 
 <script>
-import ChatMessage from './components/ChatMessage.vue'
-import Container from './components/Container.vue'
-import ChatWindow from './components/ChatWindow'
+import ChatMessage from "./components/ChatMessage.vue";
+import Container from "./components/Container.vue";
+import ChatWindow from "./components/ChatWindow";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Container,
     ChatMessage,
-    ChatWindow
+    ChatWindow,
   },
-  data(){
-   // messages:[
-   //   {username: 'Ivan', datetime: '21.312.312' , text: 'dsdsdsd'},
-   //   {username: 'Ivan', datetime: '21.312.312' , text: 'dsdsdsd'}
-   // ]
-
+  data() {
+    return {
+      messages: [
+        { username: "Ivan", datetime: "21.312.312", text: "dsdsdsd" },
+        { username: "Ivan", datetime: "21.312.312", text: "dsdsdsd" },
+      ],
+    };
   },
-  methods:{
-    getMessages(){
-      this.axios.get('http://37.77.104.246/api/chat/getmessages.php')
-      .then((response)=>{
-        this.messages=response.data;
-      })
-    }
+  methods: {
+    getMessages() {
+      this.axios
+        .get("http://37.77.104.246/api/chat/getmessages.php")
+        .then((response) => {
+          this.messages = response.data;
+        });
+        
+    },
+    
   },
-  mounted(){
-     this.getMessages();
-  }
-}
+  mounted() {
+    this.getMessages();
+  },
+};
 </script>
 
 <style>
 #app {
-
-body {
-  margin: 0;
-  background-color: #f9f9fa;
-}
-
+  body { 
+    margin: 0;
+    background-color: #f9f9fa;
+  }
 }
 </style>
